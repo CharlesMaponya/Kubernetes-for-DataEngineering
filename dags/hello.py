691a2dbf4a0f259d7ea64bb9e1abeeb55bf19efe
CharlesMaponya <maponyacl@gmail.com>
2025-07-08 16:33:36 +0200

    Initial commit

---
+++ dags/hello.py
@@ -0,0 +1,29 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args ={
+    'owner':'daemon',
+    'start_date': datetime(2025,1,25),
+    'catchup': False
+}
+
+
+dag = DAG(
+    'Hello World',
+    default_args= default_args,
+    schedule= timedelta(days=1)
+)
+task1 = BashOperator(
+    task_id='hellow owrld',
+    bash_command='echo "Hello Daemon"'
+    dag = dag
+)
+
+task2 = BashOperator(
+    task_id='hellow world',
+    bash_command='echo "Hello World"'
+    dag = dag
+)
+
+task1 >> task2
